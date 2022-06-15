751bae8581511195249090d52fb0008689e36993
soni240 <s.fegade21@gmail.com>
2022-06-15 23:15:42 +0530

    [Gunavanti]javascript programming construct

---
+++ DAY 40 assignmnent/addtworandomdice.js
@@ -0,0 +1,8 @@
+console.log("enter the dice number1");
+var dice1=10;
+
+console.log("enter the dice number2");
+var dice2=20;
+
+var add1 = dice1 + dice2;
+console.log("add of number:", +add1);
---
+++ DAY 40 assignmnent/random1to6.js
@@ -0,0 +1,2 @@
+let num1=Math.floor( (Math.random() % 6 ) + 1);
+console.log("random1to6",{num1});
